ba3f6ae56670c6690f1049a2d46c4ebc0e6a9dac
github_akeenola <akinola.olabisi@gmail.com>
2024-07-11 05:37:30 -0700

    responsive mobile version

--- action.js
+++ action.js
@@ -2,4 +2,4 @@
     let d = new Date();
   
     days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    document.getElementById("date").innerHTML="<p> &copy; Olabisi Akinola. Today, "+days[d.getDay()]+d.getUTCDate()+"</p>";
+    document.getElementById("date").innerHTML="<p> &copy; Olabisi Akinola. Today, "+days[d.getDay()]+d.toUTCString()+"</p>";
